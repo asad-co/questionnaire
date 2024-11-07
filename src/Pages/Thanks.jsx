df1b1bb3e1b83e643871370bc1f9612c2d1d8f5f
asad-co <realhk98000@gmail.com>
2024-11-07 16:30:44 +0500

    [Feat] thanks page added

--- src/Pages/Thanks.jsx
+++ src/Pages/Thanks.jsx
@@ -1,10 +1,43 @@
+import Main from "../Components/Box/Main"
+import SubMain from "../Components/Box/SubMain"
+import MainHeading from "../Components/Text/MainHeading"
+import SubHeading from "../Components/Text/SubHeading"
+import ArrowSVG from "../assets/svgs/ArrowSVG"
+import PrimaryButton from "../Components/Button/PrimaryButton"
+import BoxTitle from "../Components/Text/BoxTitle"
+import ComponentBox from "../Components/Box/ComponentBox"
+import { useNavigate } from "react-router-dom"
 
 const Thanks = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const navigate = useNavigate()
+    return (
+        <Main>
+            <SubMain rightAligned={true}>
+                <ComponentBox isWelcome={true}>
+                    <MainHeading>Thank you</MainHeading>
+                    <SubHeading> for your feedback!</SubHeading>
+                    <div className="d-flex justify-content-between w-75">
+                        <PrimaryButton
+                            onClick={() => { navigate("/score") }}
+                            theme={"secondary"}
+                            className={"d-flex justify-content-between align-items-center gap-4 py-3 px-4"}>
+                            <ArrowSVG direction={"left"} />
+                            <BoxTitle>Back</BoxTitle>
+                        </PrimaryButton>
+
+                        <PrimaryButton
+                            onClick={() => { navigate("/") }}
+                            theme={"tertiary"}
+                            className={"d-flex justify-content-between align-items-center gap-4 py-3 px-4"}>
+                            <BoxTitle>Back to Home</BoxTitle>
+                            <ArrowSVG direction={"right"} />
+                        </PrimaryButton>
+
+                    </div>
+                </ComponentBox>
+            </SubMain>
+        </Main>
+    )
 }
 
 export default Thanks

0c261d43af57d659ee34f08eccb9d31830c4f902
asad-co <realhk98000@gmail.com>
2024-11-07 22:05:25 +0500

    [chores] code clean

--- api/index.js
+++ api/index.js
@@ -1,14 +1,9 @@
 const connectToMongo = require('./db');
-const { createClient } = require('@supabase/supabase-js');
 const express = require('express')
 const cors = require('cors')
 const port = process.env.SERVER_PORT
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_PUBLIC_ANON_KEY;
 
 connectToMongo();
-const supabase = createClient(supabaseUrl, supabaseKey);
-module.exports = { supabase };
 
 const app = express()
 app.use(cors())
